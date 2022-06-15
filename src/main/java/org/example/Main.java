394dfeaade1fa5238d0c11b8d16b0f387c6a4e4e
Oleh Shvaiko <66371852+Vinnialf@users.noreply.github.com>
2022-06-15 19:43:47 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int i = 9;
+        int i = 8;
         int factorial =1 ;
         for (int j=1; j<=i; j++) {
             factorial = factorial*j;
